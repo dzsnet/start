1e8f354f1cfefafa5ce3aa288156beca0eeb54fe
dzsnet <35575153+dzsnet@users.noreply.github.com>
2025-02-14 22:54:09 +0100

    Update c-salt.ps1

--- dl/c-salt.ps1
+++ dl/c-salt.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy Bypass
 $complower = $env:computername.ToLower()
-choco install saltminion --yes --no-progress -r --installargs "/S /minion-name=$complower /master=saltmaster1.dzsnet.com,saltmaster2.dzsnet.com,saltmaster3.dzsnet.com /start-service=1"
+choco install saltminion --yes --no-progress -r --installargs "/S /minion-name=$complower /master=saltmaster1.dzsnet.com,saltmaster2.dzsnet.com,saltmaster3.dzsnet.com,saltmaster4.dzsnet.com  /start-service=1"
